7cf51fb3e84c07cf63adcd583778ebd0b7d565ba
adhimaspns <adhimaspnsmjr1@gmail.com>
2021-03-21 17:34:13 +0700

    desain sidebar

---
+++ beranda.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Beranda</title>
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+  <?php
+    include 'frontend/layouts/sidebar.php';
+  ?>
+
+  <div class="content">
+    <h1>Sistem Pendataan Gudang</h1>
+    <!-- <h2 class="teks-rata-kanan margin-top-50">Beranda</h2> -->
+    <p>This example use media queries to transform the sidebar to a top navigation bar when the screen size is 700px or less.</p>
+    <p>We have also added a media query for screens that are 400px or less, which will vertically stack and center the navigation links.</p>
+    <h3>Resize the browser window to see the effect.</h3>
+  </div>
+
+</body>
+</html>
---
+++ frontend/layouts/sidebar.php
@@ -0,0 +1,7 @@
+<div class="sidebar">
+  <a class="<?php if($_GET['page'] == 'beranda'){  ?>active <?php }?>" href="/SistemPendataanGudang/beranda.php?page=beranda">Beranda</a>
+  <a href="#news">Barang Masuk</a>
+  <a href="#contact">Barang Keluar</a>
+  <a href="#about">Laporan</a>
+  <a href="#about">Log Out</a>
+</div>
