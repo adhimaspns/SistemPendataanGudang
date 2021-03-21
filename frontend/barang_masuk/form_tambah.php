160b6a7c82bc024631abcdbc812a2c311a7e9ffd
adhimaspns <adhimaspnsmjr1@gmail.com>
2021-03-21 19:17:11 +0700

    melanjutkan desain form pada modul pendataan barang masuk

--- frontend/barang_masuk/form_tambah.php
+++ frontend/barang_masuk/form_tambah.php
@@ -20,8 +20,20 @@
     <div class="kotak-form">
       <h2>Form Tambah Barang Masuk</h2>
       <form action="/action_page.php">
-        <label for="fname">First Name</label>
-        <input type="text" class="form" name="firstname" placeholder="Your name..">
+        <label>Nama Barang</label>
+        <input type="text" class="form" name="" placeholder="Beras">
+
+        <label>Qty</label>
+        <input type="number" class="form" name="" placeholder="1000">
+
+        <label>Satuan</label>
+        <input type="text" class="form" name="" placeholder="Pcs/Kg/Ton/Dll">
+
+        <label>Pengirim</label>
+        <input type="text" class="form" name="" placeholder="Nama PT/Instansi/Dll">
+
+        <label>Harga Satuan</label>
+        <input type="text" class="form" name="" placeholder="15.000">
 
         <input type="submit" class="form-tombol" value="Submit">
       </form>
